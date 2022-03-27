6a4fe85c8bdeed83d57d51f6475681f8315f408d
Heropowerbrine <78842166+Heropowerbrine@users.noreply.github.com>
2022-03-27 16:10:45 +0800

    Update MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -13,9 +13,6 @@ class MusicBeatSubstate extends FlxSubState
 	public function new()
 	{
 		super();
-		#if android
-	        addVirtualPad(FULL, A_B);
-                #end
 	}
 
 	private var lastBeat:Float = 0;
