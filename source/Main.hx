7a3430e5f1f7ff2338a56f9cb65f3de6f7da7828
Heropowerbrine <78842166+Heropowerbrine@users.noreply.github.com>
2022-03-27 18:47:45 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -85,12 +85,6 @@ class Main extends Sprite
 
 		addChild(game);
 
-		#if !android
-		fpsCounter = new FPS(10, 3, 0xFFFFFF);
-		addChild(fpsCounter);
-		toggleFPS(FlxG.save.data.fps);
-
-		#end
 	}
 
 	var game:FlxGame;
